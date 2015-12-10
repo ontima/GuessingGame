c9be3e761d34cdc75e33f87d9b8ceb7b0dfc62e8
Ontima Chung <ontimachung@Ontimas-MacBook-Pro.local>
2015-12-10 13:54:16 -0500

    added screen updates and bug fixes

--- js/guessingGame.js
+++ js/guessingGame.js
@@ -7,24 +7,25 @@ var guessArray = [];
 var guessesLeft = 5;
 
 
-generateWinningNumber();
+winningNumber = generateRandomNumber();
 
 /* **** Guessing Game Functions **** */
 
 // Generate the Winning Number
 
-function generateWinningNumber(){
+function generateRandomNumber(){
 	var min = 1;
 	var max  = 100;
-	winningNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-	console.log("winnging number is: " + winningNumber);
+	var num = Math.floor(Math.random() * (max - min + 1)) + min;
+	//console.log("winnging number is: " + num);
+	return num;
 }
 
 // Fetch the Players Guess
 
 function playersGuessSubmission(){
 	playersGuess = +$('#guess').val();
-	console.log("guess is: " + playersGuess);
+	//console.log("guess is: " + playersGuess);
 	$('#guess').val("Enter a number 1-100");
 	checkGuess();
 }
@@ -47,15 +48,16 @@ function checkGuess(){
 		$('#results').text("Invalid number. Enter a number between 1-100");
 	} else {
 		if (playersGuess === winningNumber) {
-			console.log("Player wins");
+			//console.log("Player wins");
 			clearTextFields();
+			changeBgTextColors("yellow", "red");
 			$('#results').text("You win!");
 		} else if (guessArray.indexOf(playersGuess) > -1) {
-			console.log("You've already guess " + playersGuess);
+			//console.log("You've already guess " + playersGuess);
 			clearTextFields();
 			$('#results').text("You've already guess " + playersGuess);
 		} else {
-			console.log("Incorrect guess");
+			//console.log("Incorrect guess");
 			guessArray.push(playersGuess);
 			guessesLeft--;
 			clearTextFields();
@@ -63,6 +65,7 @@ function checkGuess(){
 				$('#results').text("Try Again.  " + guessesLeft + " guesses left.");
 				guessMessage();
 			} else {
+				changeBgTextColors("red", "black");
 				$('#results').text("You lost!");
 			}
 		}
@@ -71,8 +74,8 @@ function checkGuess(){
 
 function guessMessage() {
 	var diff = Math.abs(playersGuess - winningNumber);
-	console.log("win number is " + winningNumber);
-	console.log("diff is " + diff);
+	//console.log("win number is " + winningNumber);
+	//console.log("diff is " + diff);
 	var rounded = Math.ceil(diff / 10) * 10;
 	var highlow = lowerOrHigher();
 	var msg = "Your guess is " + highlow + " and within " + rounded + " digits of the winning number";
@@ -82,8 +85,8 @@ function guessMessage() {
 // Create a provide hint button that provides additional clues to the "Player"
 
 function provideHint(){
-	var num1 = generateWinningNumber();
-	var num2 = generateWinningNumber();
+	var num1 = generateRandomNumber();
+	var num2 = generateRandomNumber();
 	clearTextFields();
 	$('#hint').text("One of these is the winning number: " + num1 + "," + winningNumber + "," + num2);
 }
@@ -94,13 +97,19 @@ function playAgain(){
 	clearTextFields();
 	guessesLeft = 5;
 	guessArray = [];
-	generateWinningNumber();
+	winningNumber = generateRandomNumber();
+	changeBgTextColors("white", "#747704");
 }
 
 function clearTextFields() {
 	$('#results').text("");
 	$('#feedback').text("");
 	$('#hint').text("");
+}
+
+function changeBgTextColors(c1, c2) {
+	$('#resultdiv').css({"background-color":c1});
+	$('#results').css({"color":c2});
 }
 
 /* **** Event Listeners/Handlers ****  */
